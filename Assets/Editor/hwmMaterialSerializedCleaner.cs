e05da9bc2e8d07ef711c2f54b93e149718e0b38e
huangwwmm <hwm@huangwm.com>
2019-06-13 10:32:19 +0800

    add assetrelations

--- Assets/Editor/hwmMaterialSerializedCleaner.cs
+++ Assets/Editor/hwmMaterialSerializedCleaner.cs
@@ -1,12 +1,17 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
 public class hwmMaterialSerializedCleaner : EditorWindow
 {
-	[MenuItem("Custom/Utility/Material Serialized Cleaner")]
+	private const string TOOL_NAME = "Material Serialized Cleaner";
+	private const long UPDATE_PROGRESSBAR_MILLISECONDS_INTERVAL = 500;
+
+	[MenuItem("Custom/Utility/" + TOOL_NAME)]
 	public static void Init()
 	{
-		GetWindow<hwmMaterialSerializedCleaner>("Material Serialized Cleaner");
+		GetWindow<hwmMaterialSerializedCleaner>(TOOL_NAME);
 	}
 
 	protected void OnGUI()
@@ -14,24 +19,50 @@ public class hwmMaterialSerializedCleaner : EditorWindow
 		if (GUILayout.Button("Clear all unused material serialized")
 			&& EditorUtility.DisplayDialog("提示", "确定要执行这个操作？这个操作可能需要数分钟", "确认", "取消"))
 		{
-			int processMaterialCount = 0;
-			string[] materialGuids = AssetDatabase.FindAssets("t:Material");
-			for (int iGuid = 0; iGuid < materialGuids.Length; iGuid++)
-			{
-				Material iterMaterial = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(materialGuids[iGuid]), typeof(Material)) as Material;
-				if (iterMaterial != null)
-				{
-					SerializedObject iterSerializedObject = new SerializedObject(iterMaterial);
-					bool hasUnused = false;
-					hasUnused |= ProcessProperties(iterMaterial, iterSerializedObject, "m_SavedProperties.m_TexEnvs");
-					hasUnused |= ProcessProperties(iterMaterial, iterSerializedObject, "m_SavedProperties.m_Floats");
-					hasUnused |= ProcessProperties(iterMaterial, iterSerializedObject, "m_SavedProperties.m_Colors");
-					iterSerializedObject.ApplyModifiedProperties();
-					processMaterialCount += hasUnused ? 1 : 0;
-				}
-			}
-			EditorUtility.DisplayDialog("提示", string.Format("处理了{0}个Material文件", processMaterialCount), "确认");
+			hwmEditorCoroutine.Start(ClearAllUnusedMaterialSerialized());
 		}
+	}
+
+	private IEnumerator ClearAllUnusedMaterialSerialized()
+	{
+		int processMaterialCount = 0;
+		System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+		stopwatch.Start();
+		long lastProgressBarMilliseconds = 0;
+		string[] materialGuids = AssetDatabase.FindAssets("t:Material");
+		for (int iGuid = 0; iGuid < materialGuids.Length; iGuid++)
+		{
+			if (stopwatch.ElapsedMilliseconds - lastProgressBarMilliseconds > UPDATE_PROGRESSBAR_MILLISECONDS_INTERVAL)
+			{
+				lastProgressBarMilliseconds = stopwatch.ElapsedMilliseconds;
+				if (UpdateCancelableProgressBar(string.Format("Cleaning up {0}/{1}", iGuid, materialGuids.Length), (float)iGuid / materialGuids.Length))
+				{
+					yield break;
+				}
+				AssetDatabase.SaveAssets();
+			}
+
+			Material iterMaterial = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(materialGuids[iGuid]), typeof(Material)) as Material;
+			if (iterMaterial != null)
+			{
+				SerializedObject iterSerializedObject = new SerializedObject(iterMaterial);
+				bool hasUnused = false;
+				hasUnused |= ProcessProperties(iterMaterial, iterSerializedObject, "m_SavedProperties.m_TexEnvs");
+				hasUnused |= ProcessProperties(iterMaterial, iterSerializedObject, "m_SavedProperties.m_Floats");
+				hasUnused |= ProcessProperties(iterMaterial, iterSerializedObject, "m_SavedProperties.m_Colors");
+				iterSerializedObject.ApplyModifiedProperties();
+				processMaterialCount += hasUnused ? 1 : 0;
+			}
+		}
+		yield return null;
+		EditorUtility.ClearProgressBar();
+		stopwatch.Stop();
+		EditorUtility.DisplayDialog(TOOL_NAME, string.Format("Cleaned up {0} material in {1} ms", processMaterialCount, stopwatch.ElapsedMilliseconds), "确认");
+	}
+
+	private bool UpdateCancelableProgressBar(string text, float progress)
+	{
+		return EditorUtility.DisplayCancelableProgressBar(TOOL_NAME, text, progress);
 	}
 
 	private bool ProcessProperties(Material material, SerializedObject serializedObject, string path)
